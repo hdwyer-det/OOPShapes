14141f51615fe11e25492f58a93ec144604bed26
hdwyer-det <harley.dwyer5@det.nsw.edu.au>
2026-05-06 09:29:19 +1000

    Added triangle and circle classes

--- edToolOOP.py
+++ edToolOOP.py
@@ -1,5 +1,7 @@
 # This file is to demonstrate solving problems similar to Task 1 using an OOP
 
+import math
+
 # This is the rectangle class
 class Rectangle():
 
@@ -17,6 +19,49 @@ class Rectangle():
         self.length = length
         self.area = self.calculateArea()
 
+    def setWidth(self, width):
+        self.width = width
+        self.area = self.calculateArea()
+
+
+# This is the triangle class
+class Triangle():
+
+    # The initialisation method is called each time
+    #  a new object is instantiated
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+        self.area = self.calculateArea()
+
+    def calculateArea(self):
+        return 0.5 * self.base * self.height
+
+    def setBase(self, base):
+        self.base = base
+        self.area = self.calculateArea()
+
+    def setHeight(self, height):
+        self.height = height
+        self.area = self.calculateArea()
+
+# This is the circle class
+class Circle():
+
+    # The initialisation method is called each time
+    #  a new object is instantiated
+    def __init__(self, radius):
+        self.radius = radius
+        self.area = self.calculateArea()
+
+    def calculateArea(self):
+        return math.pi * self.radius **2 
+
+    def setRadius(self, radius):
+        self.radius = radius
+
+
+
 
 # The main program
 rect1 = Rectangle(10, 3)
@@ -31,3 +76,9 @@ print(rect2.area)
 # print(rect2.area)
 rect2.setLength(100)
 print(rect2.area)
+
+tri1 = Triangle(10, 2)
+print(f"Triangle area is: {tri1.area}")
+
+circ1 = Circle(5)
+print(f"Circle area is {circ1.area}")
