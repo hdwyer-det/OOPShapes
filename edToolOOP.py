baea9fdd0afb41fe5150fcd0ef9b04981c995865
hdwyer-det <harley.dwyer5@det.nsw.edu.au>
2026-05-07 10:40:36 +1000

    added parent TwoDShape class

--- edToolOOP.py
+++ edToolOOP.py
@@ -2,16 +2,36 @@
 
 import math
 
+# This is the parent two dimensional shape class
+class TwoDShape():
+    def __init__(self):
+        self.area = None
+
+    # This method will need to be overwritten on each child class
+    # as the will have different formulas for calculating the area
+    def calculateArea(self):
+        return None
+    
+    # This method will only be defined on the parent class
+    def getArea(self):
+        return self.area
+
+
 # This is the rectangle class
-class Rectangle():
+class Rectangle(TwoDShape):
 
     # The initialisation method is called each time
     #  a new object is instantiated
     def __init__(self, rectLength, rectWidth):
+
+        # Call the init method on the parent class
+        super().__init__()
+
         self.length = rectLength
         self.width = rectWidth
         self.area = self.calculateArea()
 
+    # This is an overwride of the calculateArea method
     def calculateArea(self):
         return self.length * self.width
 
@@ -65,20 +85,20 @@ class Circle():
 
 # The main program
 rect1 = Rectangle(10, 3)
-print(rect1.area)
+print(rect1.getArea())
 
 rect2 = Rectangle(3, 5)
-print(rect2.area)
+print(rect2.getArea())
 
 # rect2.length = 100
 # print(rect2.area)
 # rect2.area = rect2.calculateArea()
 # print(rect2.area)
 rect2.setLength(100)
-print(rect2.area)
+print(rect2.getArea())
 
 tri1 = Triangle(10, 2)
-print(f"Triangle area is: {tri1.area}")
+print(f"Triangle area is: {tri1.getArea()}")
 
 circ1 = Circle(5)
-print(f"Circle area is {circ1.area}")
+print(f"Circle area is {circ1.getArea()}")
